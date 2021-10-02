b2b5263355bbe7cc40f8dee7583fdae291543821
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-10-03 02:26:35 +0700

    Add files via upload

---
+++ Information.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Information
+    {
+        private string username, password;
+        private int type, id;
+
+        public Information(string username, string password, int type, int id)
+        {
+            this.username = username;
+            this.password = password;
+            this.type = type;
+            this.id = id;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public int Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
