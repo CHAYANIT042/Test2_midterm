b1c21406817fcc94907dec53a9d6976ad7220fb6
CHAYANIT042 <78426311+CHAYANIT042@users.noreply.github.com>
2021-10-03 02:26:07 +0700

    Add files via upload

---
+++ Books.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Books
+    {
+        private string[] booknames;
+
+        public Books()
+        {
+            booknames = new string[] { "Now I understand", "Revolutionary Wealth", "Six Degrees", "Les Vacances" };
+        }
+
+        public string[] GetBookList()
+        {
+            return booknames;
+        }
+    }
+}
